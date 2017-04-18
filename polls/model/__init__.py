31bc01b0e5a5f967d593c74d3092978e6790c63f
vinh-nguyen <nguyendangvinh.92dn@gmail.com>
2017-04-18 15:07:23 +0700

    init 2

--- polls/model/__init__.py
+++
@@ -1,5 +0,0 @@
-from polls.model.question import Question
-from polls.model.choice import Choice
-from polls.model.blog import Blog
-from polls.model.author import Author
-from polls.model.entry import Entry
